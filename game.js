6278bbbe466ae63188d9a9968c137b70450583b5
ahmedysf100 <108271774+ahmeddysf100@users.noreply.github.com>
2022-08-09 12:51:44 +0300

    .

--- game.js
+++ game.js
@@ -1,15 +1,33 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequense() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3")
+    playSound(randomChosenColor);
+};
+$(".btn").click(handler);
+
+function handler() {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+};
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3")
     audio.play();
-}
-$("button").click(function (e) { 
-    e.preventDefault();
-    
-});
+};
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(remove, 150);
+
+    function remove() {
+        $("#" + currentColor).removeClass("pressed");
+    };
+};
